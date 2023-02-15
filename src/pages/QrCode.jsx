32c0b5059942aeeac0275d2e8849c4e67c5b7155
Erick Leonardo Weil <erickweil2@gmail.com>
2023-02-15 18:53:56 -0400

    Teste QR Code

--- src/pages/QrCode.jsx
+++ src/pages/QrCode.jsx
@@ -1,7 +1,82 @@
-import LayoutTituloConteudo from "../components/Coisas/LayoutTituloConteudo";
+// https://blog.minhazav.dev/Qr-code-and-barcode-scanner-in-react/
+import { Html5QrcodeScanner } from "html5-qrcode";
+import React from 'react';
 
-const QrCode = () => {
-    return <LayoutTituloConteudo  titulo="QrCode" />;
+const qrcodeRegionId = "html5qr-code-full-region";
+
+class Html5QrcodePlugin extends React.Component {
+    render() {
+        return <div id={qrcodeRegionId} />;
+    }
+
+    componentWillUnmount() {
+        // TODO(mebjas): See if there is a better way to handle
+        //  promise in `componentWillUnmount`.
+        this.html5QrcodeScanner.clear().catch(error => {
+            console.error("Failed to clear html5QrcodeScanner. ", error);
+        });
+    }
+
+    componentDidMount() {
+        // Creates the configuration object for Html5QrcodeScanner.
+        function createConfig(props) {
+            var config = {};
+            if (props.fps) {
+            config.fps = props.fps;
+            }
+            if (props.qrbox) {
+            config.qrbox = props.qrbox;
+            }
+            if (props.aspectRatio) {
+            config.aspectRatio = props.aspectRatio;
+            }
+            if (props.disableFlip !== undefined) {
+            config.disableFlip = props.disableFlip;
+            }
+            return config;
+        }
+
+        var config = createConfig(this.props);
+        var verbose = this.props.verbose === true;
+
+        // Suceess callback is required.
+        if (!(this.props.qrCodeSuccessCallback )) {
+            throw "qrCodeSuccessCallback is required callback.";
+        }
+
+        this.html5QrcodeScanner = new Html5QrcodeScanner(
+            qrcodeRegionId, config, verbose);
+        this.html5QrcodeScanner.render(
+            this.props.qrCodeSuccessCallback,
+            this.props.qrCodeErrorCallback);
+    }
 };
-  
+
+class QrCode extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // This binding is necessary to make `this` work in the callback.
+        this.onNewScanResult = this.onNewScanResult.bind(this);
+    }
+
+    render() {
+        return (<div>
+            <h1>Html5Qrcode React example!</h1>
+            <Html5QrcodePlugin 
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={this.onNewScanResult}/>
+        </div>);
+    }
+
+    onNewScanResult(decodedText, decodedResult) {
+        // Handle the result here.
+        return (
+            <p>{decodedText}</p>
+        )
+    }
+};
+
 export default QrCode;
